ef5552af2bd55539cc1d53d71b953acd468fc2a2
Asaf Cohen <asafc@me.com>
2013-11-02 22:30:43 +0200

    some more aliases

---
+++ mac/project_settings.sh
@@ -0,0 +1,8 @@
+export DJANGO_SETTINGS_MODULE="pricetune.settings"
+# export DJANGO_SETTINGS_MODULE="logintool.settings"
+
+export PYTHONPATH="$PYTHONPATH:/Users/asafc/projects/price_server/pricetune/"
+export PYTHONPATH="$PYTHONPATH:/Users/asafc/projects/price_server/django_apps/django-nvd3/"
+export PYTHONPATH="$PYTHONPATH:/Users/asafc/projects/"
+export PYTHONPATH="$PYTHONPATH:/Users/asafc/projects/dyadic/logintool/"
+
